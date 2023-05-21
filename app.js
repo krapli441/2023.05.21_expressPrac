0ec9a4b979f5e68c1400d0d386fba7a4d6dad4bf
krapli441 <tlfqj1208@gmail.com>
2023-05-21 19:45:29 +0900

    path를 import하여 __dirname 변수를 선언해주고, 404 페이지와 500페이지 미들웨어를 최하단으로 이동.

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express, { response } from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 2080;
+const __dirname = path.resolve();
+
+// index.html과 about.html 경로 생성
+
+app.get("/", (req, res) => {
+  res.type("text/html");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.type("text/html");
+  res.sendFile(__dirname + "/about.html");
+});
 
 // 404 페이지 작성
-
 app.use((req, res) => {
   res.type("text/plain");
   res.status(404);
@@ -18,18 +31,6 @@ app.use((err, req, res, next) => {
   res.type("text/plain");
   res.status(500);
   res.send("500 - Internal Server Error");
-});
-
-// index.html과 about.html 경로 생성
-
-app.get("/", (req, res) => {
-  res.type("text/html");
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.get("/about", (req, res) => {
-  res.type("text/html");
-  res.sendFile(__dirname + "/about.html");
 });
 
 app.listen(port, () => {
